13cd3e0bf0b4c33ece62f80403125aa917f5c0f7
waghmarekalpesh944-crypto <waghmarekalpesh944@gmail.com>
2026-03-12 09:59:46 +0530

    Add files via upload

---
+++ code/kalpesh.ino
@@ -0,0 +1,21 @@
+#define my_led1 7 // global declaration
+#define my_led2 8 // global declaration
+
+void setup() { 
+serial.begin(9600);
+pinMode(my_led1,OUTPUT);
+pinMode(my_led2,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(my_led1,HIGH);
+  digitalWrite(my_led2,HIGH);
+  Serial.println("LEDs are ON");
+  delay(1000);  //1sec
+  digitalWrite(my_led1,LOW);
+  digitalWrite(my_led2,LOW);
+  Serial.println("LEDs are OFF");
+  delay(1000); 
+  
+}
